fdcc85c5e190b23f5ef0e51a029fc5393216f384
Rafael Melo <123074145+rafaelmelo2908@users.noreply.github.com>
2023-04-10 20:33:32 +0100

    Add files via upload

--- js/modules/numbers.js
+++ js/modules/numbers.js
@@ -1,37 +1,32 @@
 export default function initNumbers() {
-
-
-
-function animaNumbers(){
     const numbers = document.querySelectorAll('.data-number');
 
     numbers.forEach((number) => {
         const total = +number.innerText;
-        const increment = Math.floor(total / 100);
+        const increment = Math.floor(total / 25);
         let start = 0;
         const timer = setInterval(() => {
             start = start + increment;
             number.innerText = start;
-            if (start > total) {
-                number.innerText = total;
-                clearInterval(timer);
-            }
-        }, 25 * Math.random());
-    });
+            if (start > total){
+            number.innerText = total;
+            clearInterval(timer);
+        }
 
-}
-
-function handleMutation(mutation) {
-    if(mutation[0].target.classList.contains('active')){
-        observer.disconnect();
-        animaNumbers();
-    }
+        }, 50)
     
-}
+    })
 
-const observerTarget = document.querySelector ('.numbers');
-const observer = new MutationObserver(handleMutation);
 
-observer.observe(observerTarget, {attributes: true});
 
+
+    const observeTarget = document.querySelector('.numbers');
+    const observer = new MutationObserver(handleMutation);
+
+    observer.observe(observeTarget, {attributes: true});
 };
+
+
+
+    
+    
--- js/modules/scroll-side.js
+++ js/modules/scroll-side.js
@@ -4,27 +4,30 @@ export default function initAnimationScroll() {
 
     const sections = document.querySelectorAll('.js-scroll');
 
-    if(sections.length) {
-        const windowHalf = window.innerHeight * .6;
+  if(sections.length) {
+      const windowHalf = window.innerHeight * .6;
 
-        function animateScroll() {
-            sections.forEach((section) => {
-                const sectionTop = section.getBoundingClientRect().top;
-                const isSectionVisible = (sectionTop - windowHalf);
+      function animateScroll() {
+          sections.forEach((section) => {
+              const sectionTop = section.getBoundingClientRect().top;
+              const isSectionVisible = (sectionTop - windowHalf) <0;
 
-                if(isSectionVisible)
-                section.classList.add('active');
-                else if(section.classList.contains('active')) {
-                    section.classList.remove('active');
+              if(isSectionVisible) 
+                  section.classList.add('active');
+                  else if(section.classList.contains('active')) {
+                  section.classList.remove('active');
                 }
-            })
-        }
+              
+          })
+      }
 
-        animateScroll();
+      animateScroll();
 
-        window.addEventListener('scroll', animateScroll);
+      window.addEventListener('scroll', animateScroll);
 
-    }
+  }
 
 }
+
+initAnimationScroll();
 
